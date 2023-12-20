c43c8967438da7613e65a8548e5f7a6ae9c345d8
rexmortus <alecs@eml.cc>
2023-12-20 15:12:12 +1100

    simple implementation

--- scripts/module.js
+++ scripts/module.js
@@ -1,7 +1,40 @@
-Hooks.once('init', async function() {
+Hooks.on('renderTokenConfig', (app,[html],data) => {
+    
+    const currentOrientation = app.document.flags?.orientations?.orientation ?? ''
 
-});
+    let extra_html = `
+    <div class="form-group slim">
+        <label>Player Orientation <span class="units">(degrees)</span></label>
+        <div class="form-fields">
+            <input type="number" value="${currentOrientation}" step="any" name="flags.orientations.orientation" placeholder="0 deg">
+        </div>
+    </div>
+    `
+    html.querySelector('div[data-tab=character]').insertAdjacentHTML('beforeend', extra_html)
 
-Hooks.once('ready', async function() {
+})
 
-});
+Hooks.on('updateCombat', function(combat, html, data, anotherThing) {
+
+    if (game.settings.get('monks-common-display', 'hide-ui') === true) {
+        let token = combat.combatant.token;
+
+        let $canvas = $('canvas');
+        
+        if (token.flags.orientations?.orientation !== undefined) {
+            canvas.stage.angle = token.flags.orientations.orientation;
+        } else {
+            canvas.stage.angle = 0;
+        }
+
+        let position = canvas.tokens.get(combat.combatant.tokenId).position
+
+        // Pan to the  token
+        let x = position.x
+        let y = position.y
+        let scale = canvas.stage.scale._x
+
+        canvas.animatePan({x,y,scale})
+    }
+
+})
