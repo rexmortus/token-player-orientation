807204d2cf934bf868b78f5d35c4b918b131482c
rexmortus <alecs@eml.cc>
2023-12-21 11:37:48 +1100

    Animated

--- scripts/module.js
+++ scripts/module.js
@@ -18,24 +18,40 @@ Hooks.on('updateCombat', function(combat, html, data, anotherThing) {
 
     if (game.settings.get('monks-common-display', 'playerdata')[game.users.current._id].display === true) {
 
+        // Get the current combatant token
         let token = combat.combatant.token;
-
-        let $canvas = $('canvas');
         
-        if (token.flags.orientations?.orientation !== undefined) {
-            canvas.stage.angle = token.flags.orientations.orientation;
-        } else {
-            canvas.stage.angle = 0;
+        // Animation parameters
+        let targetRotation = (token.flags.orientations?.orientation) * (Math.PI / 180);
+        if (Number.isNaN(targetRotation)) {
+            targetRotation = 0;
         }
+        const currentRotation = canvas.app.stage.rotation;
 
-        let position = canvas.tokens.get(combat.combatant.tokenId).position
+        let duration = 1; // duration in seconds
+        let rotationPerFrame = (targetRotation - currentRotation) / (5 * duration);
 
+        function animate() {
+
+            if (canvas.app.stage.rotation != targetRotation) {
+                requestAnimationFrame(animate); // Continue animation while the condition is true
+                canvas.app.stage.rotation += rotationPerFrame;
+            }
+        }
+   
+        animate();
+        
         // Pan to the  token
+        let position = canvas.tokens.get(combat.combatant.tokenId).position
         let x = position.x
         let y = position.y
         let scale = canvas.stage.scale._x
 
-        canvas.animatePan({x,y,scale})
+        setTimeout(function() {
+            canvas.animatePan({x,y,scale})
+        },50)
+
+        
     }
 
 })
