46dcf5bf311caa6df0e22620700f289ab681811e
rexmortus <alecs@eml.cc>
2023-12-21 16:10:34 +1100

    Fix animations

--- scripts/module.js
+++ scripts/module.js
@@ -18,20 +18,50 @@ Hooks.on('updateCombat', function(combat, html, data, anotherThing) {
 
     if (game.settings.get('monks-common-display', 'playerdata')[game.users.current._id].display === true) {
 
+        function normalizeRadians(angle) {
+            // Normalize the angle between 0 and 2π
+            angle = angle % (2 * Math.PI);
+            if (angle < 0) angle += (2 * Math.PI);
+            return angle;
+        }
+        
+        function calculateShortestRotation(currentRotation, targetRotation) {
+            currentRotation = normalizeRadians(currentRotation);
+            targetRotation = normalizeRadians(targetRotation);
+            console.log(currentRotation);
+            console.log(targetRotation);
+        
+            let delta = targetRotation - currentRotation;
+            // Normalize the delta angle to be between -π and π
+            if (delta > Math.PI) delta -= 2 * Math.PI;
+            if (delta < -Math.PI) delta += 2 * Math.PI;
+        
+            return delta;
+        }
+        
         // Get the current combatant token
         let token = combat.combatant.token;
         
         // Animation parameters
         let targetRotation = (token.flags.orientations?.orientation) * (Math.PI / 180);
+
         if (Number.isNaN(targetRotation)) {
             targetRotation = 0;
         }
+
         const currentRotation = canvas.app.stage.rotation;
 
+        let delta = calculateShortestRotation(currentRotation, targetRotation);
         let duration = 1; // duration in seconds
-        let rotationPerFrame = (targetRotation - currentRotation) / (5 * duration);
+        let rotationPerFrame = delta / (5 * duration);
 
         function animate() {
+
+            // debugger;
+
+            if (currentRotation + delta >= targetRotation) {
+                targetRotation = currentRotation + delta;
+            }
 
             if (canvas.app.stage.rotation != targetRotation) {
                 requestAnimationFrame(animate); // Continue animation while the condition is true
@@ -48,7 +78,9 @@ Hooks.on('updateCombat', function(combat, html, data, anotherThing) {
         let scale = canvas.stage.scale._x
 
         setTimeout(function() {
+            canvas.app.stage.rotation = normalizeRadians(canvas.app.stage.rotation)
             canvas.animatePan({x,y,scale})
+            token.control()
         },50)
 
         
